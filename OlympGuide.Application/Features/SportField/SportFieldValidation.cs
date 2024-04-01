eb3215f214173e9af1edb220129757ac2c766367
Ilimea Gall <gallili1@students.zhaw.ch>
2024-04-01 08:57:55 +0200

    Add Validation to Controller Implemented Validation

---
+++ OlympGuide.Application/Features/SportField/SportFieldValidation.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OlympGuide.Application.Features.SportField
+{
+    public class SportFieldValidation
+    {
+        public Boolean checkSportFieldRequestDTO(CreateSportFieldRequestDTO sportFieldToCheck)
+        {
+            Boolean result = false;
+            if (sportFieldToCheck != null)
+            {
+                result = checkName(sportFieldToCheck) && checkCoordinates(sportFieldToCheck);
+
+            }
+
+            return result;
+        }
+
+        private Boolean checkName(CreateSportFieldRequestDTO sportFieldRequestDTO)
+        {
+            Boolean result = false;
+            if (sportFieldRequestDTO.Name != null && !sportFieldRequestDTO.Name.Equals(""))
+            {
+                result = true;
+            }
+            return result;
+        }
+
+        private Boolean checkCoordinates(CreateSportFieldRequestDTO sportFieldRequestDTO)
+        {
+            Boolean result = false;
+
+            if (sportFieldRequestDTO.Latitude <= 90.0 && sportFieldRequestDTO.Latitude >= -90.0)
+            {
+                if(sportFieldRequestDTO.Longitude <= 180.0 && sportFieldRequestDTO.Longitude >= -180 - 0)
+                {
+                    result = true;
+                }
+            }
+
+            return result;
+        }
+    }
+}
--- OlympGuide/Controllers/SportFieldController.cs
+++ OlympGuide/Controllers/SportFieldController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using OlympGuide.Application.Features.SportField;
 using OlympGuide.Domain.Features.SportField;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 
 namespace OlympGuide.Controllers
 {
@@ -17,6 +18,10 @@ namespace OlympGuide.Controllers
         public async Task<List<SportFieldDTO>> GetAllSportFields()
         {
             List<SportField> list = await _service.GetAllSportsField();
+            if(list == null || list.Count == 0)
+            {
+                //throw new NoSportFieldFoundException
+            }
             List<SportFieldDTO> result = _mapper.Map<List<SportField>,List<SportFieldDTO>>(list);
 
             return result;
@@ -25,7 +30,18 @@ namespace OlympGuide.Controllers
         [HttpGet("GetSportFieldByID/{id}")]
         public async Task<SportFieldDTO> GetSportFieldByID(Guid id)
         {
+            if(id == Guid.Empty)
+            {
+                //throw new InvalidParameterException
+            }
             SportField sportField = await _service.GetSportFieldByID(id);
+
+
+            if(sportField == null)
+            {
+                //throw new NoSportFieldFoundException
+            }
+
             SportFieldDTO sportFieldDTO = _mapper.Map<SportField,SportFieldDTO>(sportField);
 
             return sportFieldDTO;
@@ -34,6 +50,12 @@ namespace OlympGuide.Controllers
         [HttpPost("AddSportField")]
         public async Task<SportFieldDTO> AddSportField([FromBody] CreateSportFieldRequestDTO sportFieldToAdd)
         {
+            SportFieldValidation validation = new SportFieldValidation();
+
+            if(sportFieldToAdd == null || validation.checkSportFieldRequestDTO(sportFieldToAdd))
+            {
+                //throw new InvalidParameterException
+            }
 
             SportField sportField = await _service.AddSportField(sportFieldToAdd);
             SportFieldDTO sportFieldDTO = _mapper.Map<SportField, SportFieldDTO>(sportField);
