e171a980cc935caaac39848db63ade9a9516b2a6
Ilimea Gall <gallili1@students.zhaw.ch>
2024-04-03 16:37:19 +0200

    Naming fix

--- OlympGuide.Infrastructre/Repositories/SportFieldRepository.cs
+++ OlympGuide.Infrastructre/Repositories/SportFieldRepository.cs
@@ -5,7 +5,7 @@ namespace OlympGuide.Infrastructre.Repositories
 {
     public class SportFieldRepository(OlympGuideDBContext context) : ISportFieldRepository
     {
-        public async Task<List<SportField>> GetAllSportsField()
+        public async Task<List<SportField>> GetAllSportFields()
         {
             var sportFieldList = await context.SportFields
                 .ToListAsync();
