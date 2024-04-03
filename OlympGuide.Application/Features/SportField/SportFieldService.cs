84c42f926fb148e25f9f969c52350031c759daa1
Ilimea Gall <gallili1@students.zhaw.ch>
2024-04-03 17:10:35 +0200

    fixed wrong service implementation

--- OlympGuide.Application/Features/SportField/SportFieldService.cs
+++ OlympGuide.Application/Features/SportField/SportFieldService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Query;
 using OlympGuide.Application.Features.SportField;
 
 namespace OlympGuide.Domain.Features.SportField
@@ -12,22 +13,23 @@ namespace OlympGuide.Domain.Features.SportField
         private readonly ISportFieldRepository _repository = repository;
 
         public Task<SportField> AddSportField(CreateSportFieldRequestDTO sportFieldToAdd)
-        { 
+        {
+            if (sportFieldToAdd != null && SportFieldValidation.CheckSportFieldRequestDTO(sportFieldToAdd))
+            {
+                SportField newSportField = new SportField()
+                {
+                    Name = sportFieldToAdd.Name,
+                    Description = sportFieldToAdd.Description,
+                    Longitude = sportFieldToAdd.Longitude,
+                    Latitude = sportFieldToAdd.Latitude
+                };
 
-            if (sportFieldToAdd == null || SportFieldValidation.CheckSportFieldRequestDTO(sportFieldToAdd))
+                return _repository.AddSportField(newSportField);
+            }
+            else
             {
                 throw new ArgumentException();
             }
-
-            SportField newSportField = new SportField()
-            {
-                Name = sportFieldToAdd.Name,
-                Description = sportFieldToAdd.Description,
-                Longitude = sportFieldToAdd.Longitude,
-                Latitude = sportFieldToAdd.Latitude
-            };
-
-            return _repository.AddSportField(newSportField);
         }
 
         public Task<List<SportField>> GetAllSportFields()
