4a78c3a76034fc10513d72c24e114a299b17914d
dominik.mueller <dominik.mueller@noser.com>
2024-04-04 10:13:52 +0200

    refactoring of Program.cs

--- OlympGuide/Program.cs
+++ OlympGuide/Program.cs
@@ -1,41 +1,12 @@
-using OlympGuide.Domain.Features.SportField;
-using System.Text.Json;
-using Microsoft.EntityFrameworkCore;
-using OlympGuide.Infrastructre;
-using OlympGuide.Infrastructre.Repositories;
+using OlympGuide;
 
-var builder = WebApplication.CreateBuilder(args);
+var builder = Host.CreateDefaultBuilder(args)
+    .ConfigureWebHostDefaults(webBuilder =>
+ {
+     webBuilder.UseStartup<Startup>();
+ });
 
-// Add services to the container.
-
-builder.Services.AddControllers()
-    .AddJsonOptions(options =>
-        options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase);
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-
-builder.Services.AddScoped<ISportFieldService, SportFieldService>();
-builder.Services.AddScoped<ISportFieldRepository, SportFieldRepository>();
-
-builder.Services.AddDbContext<OlympGuideDBContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("OlympGuideDB")));
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
 
 app.Run();
---
+++ OlympGuide/Startup.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OlympGuide.Domain.Features.SportField;
+using OlympGuide.Infrastructre.Repositories;
+using OlympGuide.Infrastructre;
+using System.Text.Json;
+
+namespace OlympGuide
+{
+    public class Startup
+    {
+        private IConfiguration _configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add services to the container.
+            services.AddControllers()
+                    .AddJsonOptions(options =>
+                    {
+                        options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+                    });
+
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+            services.AddScoped<ISportFieldService, SportFieldService>();
+            services.AddScoped<ISportFieldRepository, SportFieldRepository>();
+
+            services.AddDbContext<OlympGuideDBContext>(options =>
+                options.UseNpgsql(_configuration.GetConnectionString("OlympGuideDB")));
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            // Configure the HTTP request pipeline.
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
