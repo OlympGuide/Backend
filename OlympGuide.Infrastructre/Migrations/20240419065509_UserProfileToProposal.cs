972581cde8961b389a700daeecd4cb3d8afb2f68
Ilimea Gall <gallili1@students.zhaw.ch>
2024-04-19 08:55:57 +0200

    add db migration

---
+++ OlympGuide.Infrastructre/Migrations/20240419065509_UserProfileToProposal.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OlympGuide.Infrastructre.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserProfileToProposal : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "ix_sport_field_proposals_user_id",
+                table: "sport_field_proposals",
+                column: "user_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_sport_field_proposals_users_user_id",
+                table: "sport_field_proposals",
+                column: "user_id",
+                principalTable: "users",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_sport_field_proposals_users_user_id",
+                table: "sport_field_proposals");
+
+            migrationBuilder.DropIndex(
+                name: "ix_sport_field_proposals_user_id",
+                table: "sport_field_proposals");
+        }
+    }
+}
