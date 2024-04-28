045e60a1a4687093d3a02667f6045279bc5b755c
Ilimea Gall <gallili1@students.zhaw.ch>
2024-04-28 15:48:39 +0200

    analyzer

--- OlympGuide.Domain/Features/Reservation/ReservationType.cs
+++ OlympGuide.Domain/Features/Reservation/ReservationType.cs
@@ -5,8 +5,8 @@ namespace OlympGuide.Domain.Features.Reservation
 {
     public class ReservationType : Entity
     {
-        public Guid SportFieldId { get; set; } = new();
-        public UserProfile User { get; set; } = new();
+        public Guid SportFieldId { get; set; }
+        public required UserProfile User { get; set; }
         public DateTime Start { get; set; }
         public DateTime End { get; set; }
         public ReservationStates State { get; set; }
