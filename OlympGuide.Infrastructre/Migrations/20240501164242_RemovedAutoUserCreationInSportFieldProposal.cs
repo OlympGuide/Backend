a55554503c0cbe7527444c4552c8c5992d53dd0b
Cyrill Neuschwander <cneuschwander@swisslex.ch>
2024-05-01 18:43:00 +0200

    created new migration

---
+++ OlympGuide.Infrastructre/Migrations/20240501164242_RemovedAutoUserCreationInSportFieldProposal.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OlympGuide.Infrastructre.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemovedAutoUserCreationInSportFieldProposal : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
