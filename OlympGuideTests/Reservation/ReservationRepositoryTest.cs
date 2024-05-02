ef0c3db00cf1fc03ae12c440580cbbb642c9fd69
Ilimea Gall <gallili1@students.zhaw.ch>
2024-05-02 07:39:29 +0200

    Reservation Mapping angepasst ReservationRespository Tests implementiert

---
+++ OlympGuideTests/Reservation/ReservationRepositoryTest.cs
@@ -0,0 +1,52 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Moq;
+using OlympGuide.Domain.Features.SportFieldProposal;
+using OlympGuide.Domain.Features.User;
+using OlympGuide.Infrastructre.Repositories;
+using OlympGuide.Infrastructre;
+using OlympGuide.Domain.Features.Reservation;
+using OlympGuide.Domain.Features.SportField;
+
+namespace OlympGuideTests.Reservation
+{
+    public class ReservationRepositoryTest
+    {
+        [Fact]
+        public async Task AddSportFieldProposal_UserProfileNotDuplicatedInUserTable()
+        {
+            // Arrange
+            var options = new DbContextOptionsBuilder<OlympGuideDbContext>()
+                .UseInMemoryDatabase(databaseName: "ReservationServiceTests")
+                .Options;
+            var dbContext = new OlympGuideDbContext(options);
+            var reservationRepository = new ReservationRepository(dbContext);
+            var userRepository = new UserRepository(dbContext, new Mock<ILogger<UserRepository>>().Object);
+
+            var user = new UserProfile();
+            await userRepository.AddUser(user);
+
+            var sportField = new SportFieldType();
+
+
+            var newReservation = new ReservationType()
+            {
+                User = user,
+                SportFieldId = sportField.Id,
+                Start = DateTime.Now,
+                End = DateTime.Now,
+                State = ReservationStates.Open
+            };
+
+            // Act
+            await reservationRepository.AddReservation(newReservation);
+
+            var result = await reservationRepository.GetReservationsBySportField(sportField.Id);
+
+            // Assert
+            Assert.Equal(user, result[0].User); // Check if there is only one entry for the user ID
+        }
+    }
+}
