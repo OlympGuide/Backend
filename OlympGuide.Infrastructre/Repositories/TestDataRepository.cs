d8c5eaf45a40328c8c0100482012b8e1b4b2b67e
Cyrill Neuschwander <cneuschwander@swisslex.ch>
2024-05-03 17:36:11 +0200

    created test data repository

---
+++ OlympGuide.Infrastructre/Repositories/TestDataRepository.cs
@@ -0,0 +1,231 @@
+﻿using OlympGuide.Domain.Features.Reservation;
+using OlympGuide.Domain.Features.SportField;
+using OlympGuide.Domain.Features.SportFieldProposal;
+using OlympGuide.Domain.Features.User;
+
+namespace OlympGuide.Infrastructre.Repositories
+{
+    public class TestDataRepository(OlympGuideDbContext context)
+    {
+        private static readonly string _guidBase = "274D2AE8-9853-4D26-B22E-FE296B77A";
+        private static int _guidCounter = 0;
+
+        private static readonly object Lock = new();
+
+        private readonly Random random = new();
+
+        public async Task<int> CreateTestData()
+        {
+            var createSportFieldProposalsTask = this.CreateTestSportFieldProposals();
+
+            var sportFields = await this.CreateTestSportFields();
+            var reservations = await this.CreateTestReservations(sportFields);
+            var sportFieldProposals = await createSportFieldProposalsTask;
+
+            Task.WaitAll(
+                context.SportFieldProposals.AddRangeAsync(sportFieldProposals), 
+                context.SportFields.AddRangeAsync(sportFields), 
+                context.Reservations.AddRangeAsync(reservations));
+
+            var amountCreated = await context.SaveChangesAsync();
+            return amountCreated;
+        }
+
+        public async Task<int> DeleteTestData()
+        {
+            var sportFieldProposalsToRemove = context.SportFieldProposals
+                .Where(sfp => sfp.Id.ToString().StartsWith(_guidBase, StringComparison.InvariantCultureIgnoreCase));
+
+            var sportFieldsToRemove = context.SportFields
+                .Where(sf => sf.Id.ToString().StartsWith(_guidBase, StringComparison.InvariantCultureIgnoreCase));
+
+            var reservationsToRemove = context.Reservations
+                .Where(r => r.Id.ToString().StartsWith(_guidBase, StringComparison.InvariantCultureIgnoreCase));
+
+            context.SportFieldProposals.RemoveRange(sportFieldProposalsToRemove);
+            context.SportFields.RemoveRange(sportFieldsToRemove);
+            context.Reservations.RemoveRange(reservationsToRemove);
+
+            var amountDeleted = await context.SaveChangesAsync();
+            return amountDeleted;
+        }
+
+        #region Test Data Creation
+
+        private async Task<List<SportFieldType>> CreateTestSportFields()
+        {
+            return await Task.Run(() =>
+            {
+                var sportFields = new List<SportFieldType>
+                {
+                    new()
+                    {
+                        Id = GenerateGuid(),
+                        Name = "ASVZ Sport Center Winterthur",
+                        Description =
+                            "Der ASVZ bietet allen Hochschulangehörigen ein attraktives und vielfältiges Sportangebot, in welchem es immer wieder Neues zu entdecken gibt und Bewährtes erhalten bleibt. Im ASVZ stehen die Menschen, die Freude an der Bewegung, die Fitness, der Ausgleich zum Studium bzw. Beruf und die Verbesserung der Lebensqualität im Zentrum.",
+                        Latitude = 47.49371318423971,
+                        Longitude = 8.717093499632721,
+                        Address = "Lagerplatz 28, 8400 Winterthur"
+                    },
+                    new()
+                    {
+                        Id = GenerateGuid(),
+                        Name = "TCE Tennis Club",
+                        Description =
+                            "Wir sind ein multikultureller Tennis Club mit einer guten Durchmischung von Hobby- und Turnierspielern / -spielerinnen. Junge, auch schon etwas Betagtere und Junioren sind bei uns sehr willkommen und fühlen sich sehr bald wohl im Club. Anfänger und auch Fortgeschrittene geniessen den Sport und auch das Gesellige des Clublebens. Gerne sind auch alteingesessene Spieler bereit, mit Neumitgliedern zu spielen.",
+                        Latitude = 47.501729507221825,
+                        Longitude = 8.713984116905758,
+                        Address = "Schützenwiesenweg 6, 8400 Winterthur"
+                    },
+                    new()
+                    {
+                        Id = GenerateGuid(),
+                        Name = "Fussballplatz Schulhaus Heiligberg",
+                        Latitude = 47.49544674677959,
+                        Longitude = 8.726180906746624,
+                        Address = "Hochwachtstrasse 9, 8400 Winterthur"
+                    },
+                    new()
+                    {
+                        Id = GenerateGuid(),
+                        Name = "Fussballplatz Primarschule Schönengrund",
+                        Latitude = 47.49357557304693,
+                        Longitude = 8.737344514983741,
+                        Address = "Weberstrasse 2, 8400 Winterthur"
+                    },
+                    new()
+                    {
+                        Id = GenerateGuid(),
+                        Name = "Seilpark Winterthur",
+                        Latitude = 47.49260861980515,
+                        Longitude = 8.73556178955866,
+                        Address = "Zeughausstrasse 54, 8400 Winterthur"
+                    },
+                    new()
+                    {
+                        Id = GenerateGuid(),
+                        Name = "Basketballplatz",
+                        Latitude = 47.502103671462486,
+                        Longitude = 8.731687249268996,
+                        Address = "Trollstrasse, 8400 Winterthur"
+                    },
+                    new()
+                    {
+                        Id = GenerateGuid(),
+                        Name = "Fussballplatz",
+                        Latitude = 47.50176868409301,
+                        Longitude = 8.731603411044478,
+                        Address = "Trollstrasse, 8400 Winterthur"
+                    },
+                    new()
+                    {
+                        Id = GenerateGuid(),
+                        Name = "Volleyballplatz Rennweg",
+                        Latitude = 47.502792309674476,
+                        Longitude = 8.714411669067376,
+                        Address = "Wartstrasse 71, 8400 Winterthur"
+                    },
+                };
+
+                return sportFields;
+            });
+        }
+
+        private async Task<List<ReservationType>> CreateTestReservations(List<SportFieldType> sportFields, int amount = 30)
+        {
+            return await Task.Run(() =>
+            {
+                var reservations = new List<ReservationType>();
+
+                const int reservationPeriodHourStart = 7;
+                const int reservationPeriod = 14;
+
+                foreach (var sportField in sportFields)
+                {
+                    var currentHour = 0;
+                    var currentDay = -3;
+
+                    for (var i = 0; i < amount; i++)
+                    {
+                        var startHour = currentHour += random.Next(0, 6);
+                        var endHour = currentHour += random.Next(1, 3);
+
+                        reservations.Add(new ReservationType
+                        {
+                            Id = GenerateGuid(),
+                            SportFieldId = sportField.Id,
+                            User = new UserProfile(),
+                            Start = this.GenerateDateTimeForReservation(currentDay,
+                                reservationPeriodHourStart + startHour),
+                            End = this.GenerateDateTimeForReservation(currentDay, reservationPeriodHourStart + endHour),
+                            State = ReservationStates.Approved,
+                        });
+
+                        if (currentHour > reservationPeriod)
+                        {
+                            currentDay++;
+                            currentHour = 0;
+                        }
+                    }
+                }
+
+                return reservations;
+            });
+        }
+
+        private async Task<List<SportFieldProposalType>> CreateTestSportFieldProposals()
+        {
+            return await Task.Run(() =>
+            {
+                var sportFieldProposals = new List<SportFieldProposalType>
+                {
+                    new()
+                    {
+                        Id = GenerateGuid(),
+                        Date = DateTime.Now,
+                        User = new UserProfile(),
+                        SportFieldName = "Salzhaus",
+                        SportFieldDescription =
+                            "Nacht- und Musikclub in ehemaligem Lagerhaus mit regelmässigen Livekonzerten und Gast-DJs.",
+                        SportFieldLatitude = 47.49754039335906,
+                        SportFieldLongitude = 8.722296236745477,
+                        SportFieldAddress = "untere Vogelsangstrasse 6, 8400 Winterthur"
+                    }
+                };
+
+                return sportFieldProposals;
+            });
+        }
+
+        #endregion
+
+        #region Misc
+
+        private static Guid GenerateGuid()
+        {
+            lock (Lock)
+            {
+                var index = _guidCounter.ToString("X3");
+                
+                var newGuidString = string.Concat(_guidBase, index);
+                var newGuid = Guid.Parse(newGuidString);
+
+                _guidCounter++;
+
+                return newGuid;
+            }
+        }
+
+        private DateTime GenerateDateTimeForReservation(int day, int hour)
+        {
+            var dateTime = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 0, 0, 0)
+                .AddDays(day)
+                .AddHours(hour);
+
+            return dateTime;
+        }
+
+        #endregion
+    }
+}
---
+++ OlympGuideTests/TestData/TestDataTests.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OlympGuide.Infrastructre.Repositories;
+using OlympGuide.Infrastructre;
+
+namespace OlympGuideTests.TestData
+{
+    public class TestDataTests
+    {
+        [Fact]
+        public async Task CreateTestData_Successful_TableNotEmpty()
+        {
+            // Arrange
+            var dbContext = this.CreateDbContext();
+            var testDataRepository = new TestDataRepository(dbContext);
+            
+            // Act
+            var amountCreated = await testDataRepository.CreateTestData();
+
+            // Assert
+            Assert.True(amountCreated > 0, "no test data were created");
+
+            var reservationCount = dbContext.Reservations.Count();
+            var test = dbContext.Reservations.ToList();
+            Assert.True(reservationCount > 0, "the reservation table is empty");
+
+            var sportFieldCount = dbContext.SportFields.Count();
+            var test2 = dbContext.SportFields.ToList();
+            Assert.True(sportFieldCount > 0, "the sport field table is empty");
+
+            var sportFieldProposalCount = dbContext.SportFieldProposals.Count();
+            Assert.True(sportFieldProposalCount > 0, "the sport field proposal table is empty");
+        }
+
+        [Fact]
+        public async Task DeleteTestData_Successful_TableEmpty()
+        {
+            // Arrange
+            var dbContext = this.CreateDbContext();
+            var testDataRepository = new TestDataRepository(dbContext);
+
+            // Act
+            await testDataRepository.CreateTestData();
+            var amountDeleted = await testDataRepository.DeleteTestData();
+
+            // Assert
+            Assert.True(amountDeleted > 0, "No test data were deleted");
+
+            var reservationCount = dbContext.Reservations.Count();
+            Assert.True(reservationCount == 0, "the reservation table is not empty");
+
+            var sportFieldCount = dbContext.SportFields.Count();
+            Assert.True(sportFieldCount == 0, "the sport field table is not empty");
+
+            var sportFieldProposalCount = dbContext.SportFieldProposals.Count();
+            Assert.True(sportFieldProposalCount == 0, "the sport field proposal table is not empty");
+        }
+
+        private OlympGuideDbContext CreateDbContext() {
+            var options = new DbContextOptionsBuilder<OlympGuideDbContext>()
+                .UseInMemoryDatabase(databaseName: "TestDataTests")
+                .Options;
+            var dbContext = new OlympGuideDbContext(options);
+
+            return dbContext;
+        }
+}
+}
