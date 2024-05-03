a48efd3e675de5b6767d70505883f249cbe6b8be
Cyrill Neuschwander <cneuschwander@swisslex.ch>
2024-05-03 17:41:01 +0200

    created TestDataController

---
+++ OlympGuide/Controllers/TestDataController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OlympGuide.Infrastructre.Repositories;
+
+namespace OlympGuide.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class TestDataController(TestDataRepository testDataRepository) : Controller
+    {
+        private readonly TestDataRepository _testDataRepository = testDataRepository;
+
+        [HttpGet("")]
+        public async Task<int> CreateOrRecreateTestData()
+        {
+            await _testDataRepository.DeleteTestData();
+            var amountCreated = await _testDataRepository.CreateTestData();
+
+            return amountCreated;
+        }
+    }
+}
